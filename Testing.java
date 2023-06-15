316678405fb8ddc5eb8b06c9aa9bb9e562612c3c
MR O L  SITHOLE <22362612@live.mut.ac.za>
2023-06-15 12:17:30 +0200

    add file

---
+++ Testing.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Testing
+{
+   public static void main(String [] args) 
+   {
+      Scanner kb = new Scanner(System.in);
+      System.out.println("Hello World:");
+      }
+    }
